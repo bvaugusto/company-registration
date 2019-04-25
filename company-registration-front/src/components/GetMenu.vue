ca2892c4f8f377f6210e4ecc7bcd1a655ccd2bea
bvaugusto <PtfbGS2@!9tsal14042017>
2019-04-24 22:02:47 -0300

    Melhoria para o menu

--- company-registration-front/src/components/GetMenu.vue
+++ company-registration-front/src/components/GetMenu.vue
@@ -6,16 +6,10 @@
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu" data-widget="tree">
           <li class="header">MAIN NAVIGATION</li>
-          <li>
-            <router-link to="/">
-              <i class="fa fa-edit"></i>
-              <span>Home</span>
-            </router-link>
-          </li>
-          <li>
-            <router-link to="organizacao">
-              <i class="fa fa-edit"></i>
-              <span>Empresa</span>
+          <li v-for="route in routes">
+            <router-link :to="route.path ? route.path : '/'" v-if="route.menu">
+              <i class="fa route.icon"></i>
+              <span>{{route.title}}</span>
             </router-link>
           </li>
         </ul>
@@ -25,10 +19,12 @@
   </div>
 </template>
 <script>
+import { routes } from "../router/index.js";
+
 export default {
   name: "GetMenu",
   data() {
-    return {};
+    return { routes };
   }
 };
 </script>
--- company-registration-front/src/router/index.js
+++ company-registration-front/src/router/index.js
@@ -3,7 +3,25 @@ import Organizacao from "../components/Organizacao";
 import Home from "../components/Home";
 
 export const routes = [
-  { path: "", component: Home },
-  { path: "/organizacao", component: Organizacao },
-  { path: "/organizacao/:id/edit", component: OrganizacaoEdit }
+  {
+    path: "",
+    component: Home,
+    title: "Home",
+    icon: "fa-home",
+    menu: true
+  },
+  {
+    path: "/organizacao",
+    component: Organizacao,
+    title: "Organização",
+    icon: "fa-edit",
+    menu: true
+  },
+  {
+    path: "/organizacao/:id/edit",
+    component: OrganizacaoEdit,
+    title: "Editar Organização",
+    icon: "fa-edit",
+    menu: false
+  }
 ];
